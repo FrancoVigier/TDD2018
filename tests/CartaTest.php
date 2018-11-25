20de45fef5d107705fe692c01c6facb8711751c7
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 14:22:39 -0300

    devuelvo numero, y saco logica assert

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -4,14 +4,14 @@ use PHPUnit\Framework\TestCase;
 class CartaTest extends TestCase {
 	public function testPalo(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertEquals($carta->palo(), "Corazones" || "Treboles" || "Picas" || "Diamantes" || "Bastones" || "Copas" || "Espadas" || "Oro");
+		$this->assertTrue($carta->isPalo());
 	}
 	public function testNumero(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertFalse(isalpha($carta->numero()));
+		$this->assertFalse(is_int($carta->numero()));
 	}
 	public function testTipo(){
 		$carta = new Carta("9", "Treboles");
-		$this->assertEquals($carta->tipo(),"Poker"||"Españolas");
+		$this->assertEquals($carta->tipo(),"Poker");
 	}
 }
