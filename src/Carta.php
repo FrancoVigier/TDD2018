6fbdc3c4a5311af80454cc6ca1af63b632bea77a
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 15:40:51 -0300

    scrutinizer no servis puto

--- src/Carta.php
+++ src/Carta.php
@@ -1,57 +1,57 @@
 <?php
+
 namespace TDD;
 /*
  //clase carta, prototipos de carta
 */
+
 class Carta
-{protected $numero;
- protected $palo;
- protected $tipo;
+{
+		protected $numero;
+		protected $palo;
+		protected $tipo;
 
- 	  
-	  //@desc constructor de la clase
-	  //@param int, string numero de la carta y palo
-	  //@return
-	  
-	   public function __construct($numero = null, $palo = null) 
-	   {
-		   $this->numero = $numero;
-		   $this->palo = $palo;
-		   if ($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones") {
-		   $this->tipo = "Poker";
-	   } else {
-		$this->tipo = "Españolas";
-	}
-	}
-		
-		  //@desc devuelve el palo de la carta
-		  //@param
-		  //@return string palo de la carta
-		
+		//@desc constructor de la clase
+		//@param int, string numero de la carta y palo
+		//@return
 
-	public function palo() // devuelve el palo de la carta
-	{
-		return $this->palo;
-	}
+		public function __construct($numero = null, $palo = null) 
+		{
+			$this->numero = $numero;
+			$this->palo = $palo;
+			if ($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones") {
+				$this->tipo = "Poker";
+			} else {
+				$this->tipo = "Españolas";
+			}
+		}
+
+		//@desc devuelve el palo de la carta
+		//@param
+		//@return string palo de la carta
 
 
-		
-		  //@desc devuelve el numero de la carta
-		  //@param
-		  //@return int numero de la carta
-		 
-	public function numero() // devuelve el numero de la carta
-	{
-	return $this->numero;
-	}
+		public function palo() // devuelve el palo de la carta
+		{
+			return $this->palo;
+		}
 
-		
-		  //@desc devuelve el tipo de la carta, de poker o espaÃ±ola
-		  //@param
-		  //@return string de poker o espaÃ±ola
-		
- public function tipo()// devuelve el tipo de la carta
- {
-	return $this->tipo;
-}
+
+		//@desc devuelve el numero de la carta
+		//@param
+		//@return int numero de la carta
+
+		public function numero() // devuelve el numero de la carta
+		{
+			return $this->numero;
+		}
+
+		//@desc devuelve el tipo de la carta, de poker o espaÃ±ola
+		//@param
+		//@return string de poker o españolas
+
+		public function tipo()// devuelve el tipo de la carta
+		{
+			return $this->tipo;
+		}
 }
