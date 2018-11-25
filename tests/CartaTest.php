94e4860896e1795e6701fd99de3731eea524832c
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 14:18:52 -0300

    test de carta para palo num y tip

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -1,17 +1,17 @@
 <?php
- namespace TDD;
- use PHPUnit\Framework\TestCase;
- class MazoTest extends TestCase {
- 	public function testPalo(){
-		$carta = new Carta;
+namespace TDD;
+use PHPUnit\Framework\TestCase;
+class MazoTest extends TestCase {
+	public function testPalo(){
+		$carta = new Carta("9", "Treboles");
 		$this->assertEquals($carta->palo(), "Corazones" || "Treboles" || "Picas" || "Diamantes" || "Bastones" || "Copas" || "Espadas" || "Oro");
 	}
- 	public function testNumero(){
-		$carta = new Carta;
+	public function testNumero(){
+		$carta = new Carta("9", "Treboles");
 		$this->assertFalse(isalpha($carta->numero()));
 	}
- 	public function testTipo(){
-		$carta = new Carta;
-		$this->assertEquals($carta->tipo(),"Poker"||"Espaniola");
+	public function testTipo(){
+		$carta = new Carta("9", "Treboles");
+		$this->assertEquals($carta->tipo(),"Poker"||"Españolas");
 	}
- }
+}
