343f6b1839e22a0707048accbb9152ec81f87a68
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 15:44:28 -0300

    scrutinizer no servis

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -1,15 +1,19 @@
 <?php
 namespace TDD;
+
 use PHPUnit\Framework\TestCase;
+
 class CartaTest extends TestCase {
 	public function testPalo(){
 		$carta = new Carta(9, "Treboles");
 		$this->assertEquals($carta->palo(), "Treboles");
 	}
+
 	public function testNumero(){
 		$carta = new Carta(9, "Treboles");
 		$this->assertTrue(is_int($carta->numero()));
 	}
+	
 	public function testTipo(){
 		$carta = new Carta(9, "Treboles");
 		$this->assertEquals($carta->tipo(),"Poker");
