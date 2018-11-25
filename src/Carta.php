9f20a8811c12bda18d0761526efc8c62a3381df1
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 14:20:06 -0300

    funciones de return acorde a los test anteriores

--- src/Carta.php
+++ src/Carta.php
@@ -1,11 +1,25 @@
 
 <?php
- namespace TDD;
- class Carta {
- 	protected $numero;
+namespace TDD;
+class Carta {
+	protected $numero;
 	protected $palo;
- 	public function __construct($numero=NULL, $palo=NULL){
+	public function __construct($numero=NULL, $palo=NULL){
 		$this->numero = $numero;
 		$this->palo = $palo;
+		if($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones"){
+			$this->tipo = "Poker"
+		}else{
+			$this->tipo = "Españolas"
+		}
+	}
+	public function palo(){
+		return $this->palo;	
+	}
+	public function numero(){
+		return $this->numero;
+	}
+	public function tipo(){
+		return $this->tipo;
 	}
 }
