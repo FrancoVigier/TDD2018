2597884ed7426b4bb07bd13b743ccbe2635d3985
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 15:46:11 -0300

    ultima identacion

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -4,18 +4,18 @@ namespace TDD;
 use PHPUnit\Framework\TestCase;
 
 class CartaTest extends TestCase {
-	public function testPalo(){
-		$carta = new Carta(9, "Treboles");
-		$this->assertEquals($carta->palo(), "Treboles");
-	}
+		public function testPalo(){
+			$carta = new Carta(9, "Treboles");
+			$this->assertEquals($carta->palo(), "Treboles");
+		}
 
-	public function testNumero(){
-		$carta = new Carta(9, "Treboles");
-		$this->assertTrue(is_int($carta->numero()));
-	}
-	
-	public function testTipo(){
-		$carta = new Carta(9, "Treboles");
-		$this->assertEquals($carta->tipo(),"Poker");
-	}
+		public function testNumero(){
+			$carta = new Carta(9, "Treboles");
+			$this->assertTrue(is_int($carta->numero()));
+		}
+
+		public function testTipo(){
+			$carta = new Carta(9, "Treboles");
+			$this->assertEquals($carta->tipo(),"Poker");
+		}
 }
