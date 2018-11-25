85ed27b1a2c8960eaa43ba62cd01c1d673bb9a06
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 15:07:23 -0300

    comentar carta

--- src/Carta.php
+++ src/Carta.php
@@ -1,29 +1,40 @@
 <?php
 namespace TDD;
-class Carta {
-	protected $numero;
-	protected $palo;
-	protected $tipo;
-	public function __construct($numero=NULL, $palo=NULL){
-		$this->numero = $numero;
-		$this->palo = $palo;
-		if($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones"){
-			$this->tipo = "Poker";
-		}else{
-			$this->tipo = "Españolas";
-		}
+/**
+ clase carta, prototipos de carta
+*/
+class Carta
+{protected $numero;
+ protected $palo;
+ protected $tipo;
+
+	   public function __construct($numero = null, $palo = null) //Constructor de la clase asigna tipo y palo
+	   {
+		   $this->numero = $numero;
+           $this->palo = $palo;
+		   if ($palo == "Treboles" || $palo == "Diamantes" || $palo == "Picas" || $palo == "Corazones") {
+		   $this->tipo = "Poker";
+	   } else {
+		$this->tipo = "Españolas";
 	}
-	public function palo(){
-		return $this->palo;	
 	}
-	public function isPalo(){
-		return ($this->tipo == "Poker" || $this->tipo == "Españolas");
+
+
+	public function palo() // devuelve el palo de la carta
+	{
+		return $this->palo;
 	}
-	
-	public function numero(){
-		return $this->numero;
+
+
+
+	public function numero() // devuelve el numero de la carta
+	{
+	return $this->numero;
 	}
-	public function tipo(){
-		return $this->tipo;
-	}
+
+		
+ public function tipo()// devuelve el tipo de la carta
+ {
+	return $this->tipo;
+}
 }
