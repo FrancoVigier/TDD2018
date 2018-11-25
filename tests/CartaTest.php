aabb8c0d0e689be3ec9699b841a3f586019e0ee3
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 14:37:32 -0300

    str ->int

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -3,15 +3,15 @@ namespace TDD;
 use PHPUnit\Framework\TestCase;
 class CartaTest extends TestCase {
 	public function testPalo(){
-		$carta = new Carta("9", "Treboles");
+		$carta = new Carta(9, "Treboles");
 		$this->assertTrue($carta->isPalo());
 	}
 	public function testNumero(){
-		$carta = new Carta("9", "Treboles");
+		$carta = new Carta(9, "Treboles");
 		$this->assertFalse(is_int($carta->numero()));
 	}
 	public function testTipo(){
-		$carta = new Carta("9", "Treboles");
+		$carta = new Carta(9, "Treboles");
 		$this->assertEquals($carta->tipo(),"Poker");
 	}
 }
