ac77dc0bb52fe54dd5a4af7b0bf1f88edc983fb8
FrancoVigier <francovallejosvigier@gmail.com>
2018-11-25 14:11:37 -0300

    class carta

--- src/Carta.php
+++ src/Carta.php
@@ -1,1 +1,11 @@
 
+<?php
+ namespace TDD;
+ class Carta {
+ 	protected $numero;
+	protected $palo;
+ 	public function __construct($numero=NULL, $palo=NULL){
+		$this->numero = $numero;
+		$this->palo = $palo;
+	}
+}
